23cf73160115e3330aa131987ca1eca5bc1c6d88
alisy404 <sayyedshahbazali305@gmail.com>
2026-01-16 11:55:34 +0530

    feat: add ECR repository URL output to Terraform configuration

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -9,3 +9,8 @@ output "public_subnets" {
 output "private_subnets" {
   value = module.vpc.private_subnets
 }
+
+output "ecr_repository_url" {
+  description = "ECR repository URL"
+  value       = module.ecr.repository_url
+}
